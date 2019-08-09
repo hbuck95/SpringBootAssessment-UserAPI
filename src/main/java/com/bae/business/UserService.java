1b2bda9ca3152b1706da5d9295ae81b07db311de
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-09 10:59:26 +0100

    Added method to retrieve all users

--- src/main/java/com/bae/business/UserService.java
+++ src/main/java/com/bae/business/UserService.java
@@ -1,9 +1,13 @@
 package com.bae.business;
 
+import java.util.List;
+
 import com.bae.persistence.domain.User;
 
 public interface UserService {
-	public String createUser(String user);
+	public String createUser(User user);
+
+	public List<User> getAllUsers();
 
 	public User findUserByMemberId(int memberId);
 
